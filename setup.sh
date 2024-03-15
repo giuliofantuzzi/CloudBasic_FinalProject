9be064184434199dfde868b698063a4691c90726
giuliofantuzzi <giulio.fantuzzi01@gmail.com>
2024-03-15 14:51:38 +0100

    add setup.sh script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Start the containers in background
+docker - compose up -d
+# Set default quote-per-user
+docker exec --user www-data nextcloud php occ config:app:set files default_quota --value '2 GB'
+# Enable encryption
+docker exec --user www-data nextcloud /var/www/html/occ app:enable encryption
+docker exec --user www-data nextcloud /var/www/html/occ encryption:enable
+# Set trusted domain
+docker exec --user www-data nextcloud /var/www/html/occ config:system:set trusted_domains 1 --value=nextcloud
+# Create users and generate files
+sh ./create_usr.sh 50
+sh ./data/generate_files.sh
